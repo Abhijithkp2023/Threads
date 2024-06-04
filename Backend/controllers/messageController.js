a6b907a16924b192d2a9a8317011102a4ba14734
Abhijithkp2023 <abhijithkp642@gmail.com>
2024-06-04 18:31:36 +0530

    Get messages && Get current user conversations complete - backend

--- Backend/controllers/messageController.js
+++ Backend/controllers/messageController.js
@@ -7,7 +7,7 @@ const sendMessage = async (req,res) => {
         
         const senderId = req.user._id 
 
-        let conversation = await Conversation.findOne({participents : {$all : [senderId , recipientId]}});
+        let conversation = await Conversation.find({participents : {$all : [senderId , recipientId]}});
 
         if(!conversation) {
             conversation = new Conversation({
@@ -42,4 +42,42 @@ const sendMessage = async (req,res) => {
     }
 }
 
-export { sendMessage } 
+
+const getMessages = async (req , res) => {
+    const {otherUserId} = req.params;
+    const userId = req.user._id;
+
+    try {
+        const conversation = await Conversation.findOne({
+            participents:{$all: [userId , otherUserId]}
+        })
+
+        if(!conversation) {
+            return res.status(404).json({error: "conversation not found"})
+        }
+
+        const messaages = await Message.find({
+            conversationId : conversation._id
+        }).sort({createdAt : 1})
+
+        res.status(200).json(messaages);
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+}
+
+const getConversations = async (req , res) => {
+        const userId = req.user._id;
+    try {
+        const conversations = await Conversation.find({participents : userId}).populate({
+            path : "participents",
+            select : "username profilePic" , 
+        })
+
+        res.status(200).json(conversations)
+    } catch (error) {
+        res.status(500).json({error : error.message});
+    }
+}
+
+export { sendMessage , getMessages ,getConversations } 
